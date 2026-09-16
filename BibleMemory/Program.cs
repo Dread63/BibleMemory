9c71b9588a66abc61bb65ff46df0293de45a4595
Josh Douglas <123434357+Dread63@users.noreply.github.com>
2026-09-16 10:41:20 -0600

    Initial Commit

---
+++ BibleMemory/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+class BibleMemory
+{
+    static void Main(string[] args)
+    {
+
+
+        String[] verses = { "John 1:1-5", "Matthew 2:6-10", "Psalm 4", "Proverbs 2:5-10" };
+        Regex multiVersePattern = new Regex(@":\d[-]\d");
+        
+        IEnumerable<String> verseQuery =
+            from verse in verses
+            where multiVersePattern.IsMatch(verse)
+            select verse;
+        
+        foreach (String verse in verseQuery)
+        {
+            Console.Out.WriteLine(verse);
+        }
+    }
+}
+
